57502a7628cb5e4861c4ad15c0e19afda2dcbbcf
unknown <pragyandhar@gmail.com>
2026-02-09 23:30:08 +0530

    Modification

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def starting():
     return "I am currently doing CI/CD pipeline using GitHub Action"
 
+@app.route('/hello')
+def hello():
+    return "Hello, I am using Flask with CI/CD"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7645)
